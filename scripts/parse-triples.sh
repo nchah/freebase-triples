3026b633f399d73c4ad8a1fbf32550f56980ebd6
Niel C <nielchah@gmail.com>
2016-06-08 17:41:29 -0400

    starting bash script to parse triples

---
+++ scripts/parse-triples.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A Bash script to parse, process, clean the Freebase data dumps.
+
+# Scan through the compressed data
+# zmore freebase-rdf-latest.gz
+
+# Grep for specific terms, limit set at 5
+# zgrep 'term' -m 5 freebase-rdf-latest.gz
+
+# Pipe the data to another file
+# zgrep 'term' freebase-rdf-latest.gz > freebase-triples.txt
+
+
+
+
